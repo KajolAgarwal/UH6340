edce9031022cdbb4734a91668a144a42e749e873
KajolAgarwal <agarwal.kajol.7@gmail.com>
2015-03-02 19:40:10 -0600

    Main.java

--- src/main/java/com/UH6340/Main.java
+++ src/main/java/com/UH6340/Main.java
@@ -28,6 +28,7 @@ public class Main {
             "CREATE classlisting (classid INT, teacherid INT)",
             "INSERT INTO teachers (1, \"Bob\", \"A\")",
             "INSERT INTO teachers (2, \"Sally\", \"B\")",
+            "Insert INTO teachers (3,\"Roy\",\"C\")",
             "INSERT INTO classes (1, \"Economics\", \"1000\")",
             "INSERT INTO classes (2, \"CS\", \"1000\")",
             "INSERT INTO students (1, \"Alice\", \"C\")",
